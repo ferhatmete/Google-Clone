a7dee52d16e593bc16879c6378334f746fca1652
ferhatmete <ferhatmete9224@gmail.com>
2022-03-13 20:23:32 +0300

    fix buttons styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 //CSS
 import "./Search.css";
 //Icons
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
-function Search() {
+
+function Search({ hideButtons = false }) {
+  const [input, setInput] = useState("");
+  const history = useNavigate();
+
+  const search = (e) => {
+    e.preventDefault();
+
+    history.push(`/search`);
+  };
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
       </div>
       <div className="search__buttons">
-        <Button variant="outline">Google Search</Button>
+        <Button type="submit" onClick={search} variant="outline">
+          Google Search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 }
 
