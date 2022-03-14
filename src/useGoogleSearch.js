24f71ce74e582401cabce45a4903566db9362a18
ferhatmete <ferhatmete9224@gmail.com>
2022-03-14 15:18:19 +0300

    setup custom googleSearch hook

---
+++ src/useGoogleSearch.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+const CONTEXT_KEY = "509bb3aa6474ea123";
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {}, [term]);
+};
+
+export default useGoogleSearch;
