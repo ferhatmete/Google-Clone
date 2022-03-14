dffe96a609bb131f82ab878f6e086c6ecd44b46e
ferhatmete <ferhatmete9224@gmail.com>
2022-03-14 13:58:58 +0300

    build searchPage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+// Context API
+import { useStateValue } from "../StateProvider";
 // CSS
-import "./Home.css";
+import "./SearchPage.css";
 
 function SearchPage() {
-  return <div>SearchPage</div>;
+  const [{ term }, dispatch] = useStateValue();
+  return (
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results"></div>
+    </div>
+  );
 }
 
 export default SearchPage;
