f69abfb27535eb39c3b7c10e91d2ad688441f827
ferhatmete <ferhatmete9224@gmail.com>
2022-03-14 18:50:04 +0300

    Build item under the SearchBar Left Side

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -10,6 +10,13 @@ import { useStateValue } from "../StateProvider";
 import Search from "../components/Search";
 // CSS
 import "./SearchPage.css";
+// Icons
+import SearchIcon from "@mui/icons-material/Search";
+import Description from "@mui/icons-material/Description";
+import ImageIcon from "@mui/icons-material/Image";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import RoomIcon from "@mui/icons-material/Room";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -32,6 +39,35 @@ function SearchPage() {
         </Link>
         <div className="searchPage__headerBody">
           <Search />
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <Description />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">Maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">More</Link>
+              </div>
+            </div>
+            <div className="searchPage__optionsRight"></div>
+          </div>
         </div>
       </div>
       <div className="searchPage__results"></div>
